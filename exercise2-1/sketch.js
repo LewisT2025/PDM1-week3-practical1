d9bf0ca35931c118090dcec9e2cba503337ce5bf
LewisT2025 <lewisthornton11@gmail.com>
2025-10-08 12:24:45 +0100

    finish at home

---
+++ exercise2-1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(600,600);
+fillColour = color(random(255), random(255), random(255));
+}
+
+function draw() {
+    background(255);
+    circle(300,300,400);
+    fill(fillColour)
+    if (keyCode = 13)
+    keyPressed(UP_ARROW +- 5)
+    keyPressed(LEFT_ARROW +- 5)
+    keyPressed(DOWN_ARROW +- 5)
+    keyPressed(RIGHT_ARROW +- 5)
+}
